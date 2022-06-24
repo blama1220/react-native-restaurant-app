49d030139f3ae3a4a7a433b52f3da0d06063e2cf
Brian Lopez <brianlama1220@gmail.com>
2022-06-24 01:47:05 -0400

    updated

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 const ResultsShowScreen = ({ navigation }) => {
-  //using the id from ResultList of each item to connect with.
+  //using the id from ResultList of each item to connect with
   const id = navigation.getParam("id");
   return (
     <View>
