102feb9159bbec4717378365c8f72d6eb98226cf
Brian López <brianlama1220@gmail.com>
2022-06-24 02:21:11 -0400

    show restaurants photos on show screen and hiding title on search screen

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,16 +1,44 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, FlatList, Image } from "react-native";
+import yelp from "../api/yelp";
 
 const ResultsShowScreen = ({ navigation }) => {
+  //when we expect an object we use null as defualt to indicate there's no data available yet
+  const [result, setResult] = useState(null);
   //using the id from ResultList of each item to connect with
   const id = navigation.getParam("id");
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  if (!result) {
+    return null;
+  }
   return (
     <View>
-      <Text>Result Show</Text>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />;
+        }}
+      ></FlatList>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300,
+  },
+});
 
 export default ResultsShowScreen;
